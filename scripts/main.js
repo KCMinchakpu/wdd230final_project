c590b5af5794e6bca941d3dac0a24585d536307d
KCMinchakpu <kevinomics101@gmail.com>
2022-11-29 15:06:45 +0100

    up

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,1 @@
 
-function myFunction() {
-    var x = document.getElementById("myNav");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
